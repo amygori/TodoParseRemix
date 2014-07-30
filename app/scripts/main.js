d114f8aa22ec09577b5f2fd51c11de8d88c533ac
amygori <amydgori@gmail.com>
2014-07-30 13:26:05 -0400

    built parse object and first view

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,110 @@
 Parse.initialize("arqKTROGLD7rRXl0EWBLp1pNiA4dG8mjLrciSmsq", "KEK3MrN8zasGXua1UcPNVR6Oxfa8LyVVvGbITApO");
 
+//SINGLE TASK OBJECT
+var Task = Parse.Object.extend("Task", {
+	//this is like the model in the Backbone app
+    defaults: {
+      content: "what do you have to do?",
+      done: false
+    },
+    //setting an initial valuel: is this because it can't be empty?
+    initialize: function() {
+      if (!this.get("content")) {
+        this.set({"content": this.defaults.content});
+      }
+    },
+    //done or not?
+    toggle: function() {
+      this.save({done: !this.get("done")});
+    }
+  });
+  	// This is the transient application state, not persisted on Parse
+  var AppState = Parse.Object.extend("AppState", {
+    defaults: {
+      filter: "all"
+    }
+  });
 
+//COLLECTION
+var TaskList = Parse.Collection.extend({
+    model: Task,
 
+    // Filter down the list of all todo items that are finished.
+    done: function() {
+      return this.filter(function(todo){ return task.get('done'); });
+    },
+
+    // Filter down the list to only todo items that are still not finished.
+    remaining: function() {
+      return this.without.apply(this, this.done());
+    },
+
+    // We keep the Todos in sequential order, despite being saved by unordered
+    // GUID in the database. This generates the next order number for new items.
+    nextOrder: function() {
+      if (!this.length) return 1;
+      return this.last().get('order') + 1;
+    },
+
+    // Todos are sorted by their original insertion order.
+    comparator: function(todo) {
+      return todo.get('order');
+    }
+
+  });
+
+//VIEWS
+  var TaskView = Parse.View.extend({
+
+    tagName:  "li",
+
+    template: _.template($('#item-template').html()),
+
+    events: {
+      "click .toggle"              : "toggleDone",
+      "dblclick label.todo-content" : "edit",
+      "click .todo-destroy"   : "clear",
+      "keypress .edit"      : "updateOnEnter",
+      "blur .edit"          : "close"
+    },
+
+    initialize: function() {
+      _.bindAll(this, 'render', 'close', 'remove');
+      this.model.bind('change', this.render);
+      this.model.bind('destroy', this.remove);
+    },
+
+    render: function() {
+    	//compile the template
+      $(this.el).html(this.template(this.model.toJSON()));
+      this.input = this.$('.edit');
+      return this;
+    },
+
+    toggleDone: function() {
+      this.model.toggle();
+    },
+
+    // Switch this view into `"editing"` mode, displaying the input field.
+    edit: function() {
+      $(this.el).addClass("editing");
+      this.input.focus();
+    },
+
+    // Close the `"editing"` mode, saving changes to the todo.
+    close: function() {
+      this.model.save({content: this.input.val()});
+      $(this.el).removeClass("editing");
+    },
+
+    // If you hit `enter`, we're through editing the item.
+    updateOnEnter: function(e) {
+      if (e.keyCode == 13) this.close();
+    },
+
+    // Remove the item, destroy the model.
+    clear: function() {
+      this.model.destroy();
+    }
+
+  });
